1167380a5e8f34b50bedcea4fe85209723c3d1a1
Anjaehyun <lvet0330@gmail.com>
2022-12-15 23:26:31 +0900

    # Adapter Pattern
    Adapter parrtern
    실생활에서는 110볼트를 220볼트로 변경해주거나 반대로 해주는 돼지코가 있다
    호환성이 없는 기존 클래스의 인터페이스를 변환하여 재사용 할 수 있게 해줌
    SOLID 중에서 개방폐쇄 원칙 (OCP)를 따른다.

--- study_design/src/Main.java
+++ study_design/src/Main.java
@@ -1,5 +1,4 @@
-import com.company.design.adapter.Electronic110V;
-import com.company.design.adapter.HairDryer;
+import com.company.design.adapter.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,11 +13,27 @@ public class Main {
         System.out.println("두 객체가 동일한가?");
         System.out.println(aClient.equals(bClient));
          */
+
+        //110V 짜리 드라이기
         HairDryer hairDryer = new HairDryer();
         connect(hairDryer);
+
+        //220V 짜리 청소기
+        Cleaner cleaner = new Cleaner();
+//        connect(cleaner);
+        //어댑터에 220V짜리 청소기 넣음
+        Electronic110V adapter = new SocketAdapter(cleaner);
+        connect(adapter);
+
+        //220V 에어컨
+        AirConditioner airConditioner = new AirConditioner();
+        Electronic110V airAdapter = new SocketAdapter(airConditioner);
+        connect(adapter);
     }
 
+
     //콘센트
+    //main이 static 이라 얘도 static 으로 만들어야 함
     public static void connect(Electronic110V electronic110V) {
         electronic110V.powerOn();
     }
